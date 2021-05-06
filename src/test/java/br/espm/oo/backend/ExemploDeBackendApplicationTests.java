a5b1dbdbf69eb7e8e187f41dfc6a4863c08e13e3
Humberto Sandmann <hsandmann@ieee.org>
2021-05-06 10:14:14 -0300

    First version

---
+++ src/test/java/br/espm/oo/backend/ExemploDeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.espm.oo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploDeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
